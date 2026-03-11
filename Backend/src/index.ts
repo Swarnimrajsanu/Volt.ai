7c8901dd09fbd1a017fc203982c549968e71da7e
Swarnimrajsanu <swarnimrajsanu@gmail.com>
2026-03-12 00:47:04 +0530

    Backend done

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,33 +1,73 @@
-import * as dotenv from "dotenv";
+require("dotenv").config();
+import cors from "cors";
+import express from "express";
 import OpenAI from "openai";
+import { basePrompt as nodeBasePrompt } from "./defaults/node.js";
+import { basePrompt as reactBasePrompt } from "./defaults/react.js";
+import { BASE_PROMPT, getSystemPrompt } from "./prompt.js";
 
-dotenv.config();
-
-// Create an OpenAI instance mapped to OpenRouter
 const openai = new OpenAI({
     baseURL: "https://openrouter.ai/api/v1",
-    apiKey: process.env.OPENROUTER_API_KEY, // Make sure to add this to your .env file
-    // OpenRouter requires standard headers for ranking and features
+    apiKey: process.env.OPENROUTER_API_KEY,
     defaultHeaders: {
-        "HTTP-Referer": process.env.VITE_URL || "http://localhost:3000", // Optional, mapped to your site
-        "X-Title": "Volt.ai", // Optional, your site name
+        "HTTP-Referer": process.env.VITE_URL || "http://localhost:3000",
+        "X-Title": "Volt.ai",
     }
 });
+const app = express();
+app.use(cors())
+app.use(express.json())
 
-async function main() {
-    try {
-        const completion = await openai.chat.completions.create({
-            model: "anthropic/claude-4.5-opus",
-            messages: [
-                { role: "system", content: "You are a helpful assistant." },
-                { role: "user", content: "What is ai" }
-            ],
-        });
+app.post("/template", async (req, res) => {
+    const prompt = req.body.prompt;
 
-        console.log("Response:", completion.choices[0]?.message?.content);
-    } catch (error) {
-        console.error("Error communicating with OpenRouter:", error);
+    const response = await openai.chat.completions.create({
+        model: 'anthropic/claude-3.5-sonnet',
+        max_tokens: 200,
+        messages: [
+            { role: "system", content: "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra" },
+            { role: 'user', content: prompt }
+        ],
+    })
+
+    const answer = response.choices[0]?.message?.content || ""; // react or node
+    if (answer == "react") {
+        res.json({
+            prompts: [BASE_PROMPT, `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [reactBasePrompt]
+        })
+        return;
     }
-}
 
-main();
+    if (answer === "node") {
+        res.json({
+            prompts: [`Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`],
+            uiPrompts: [nodeBasePrompt]
+        })
+        return;
+    }
+
+    res.status(403).json({ message: "You cant access this" })
+    return;
+
+})
+
+app.post("/chat", async (req, res) => {
+    const messages = req.body.messages;
+    const response = await openai.chat.completions.create({
+        model: 'anthropic/claude-3.5-sonnet',
+        max_tokens: 8000,
+        messages: [
+            { role: "system", content: getSystemPrompt() },
+            ...messages
+        ]
+    })
+
+    console.log(response);
+
+    res.json({
+        response: response.choices[0]?.message?.content
+    });
+})
+
+app.listen(3000);
